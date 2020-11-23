73bfc33d6525c80fa71248a2b34137a66f40b2b2
chel <c.l.webster@live.com>
2020-11-23 22:09:00 +0000

    init game setup

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+    [SerializeField] private float _moveSpeed = 10f;
+
+
+    void Update() {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        if (horizontal != 0) {
+            transform.Translate(Vector3.right * horizontal * _moveSpeed * Time.deltaTime);
+        }
+
+        if (vertical != 0) {
+            transform.Translate(Vector3.up * vertical * _moveSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Raycast.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Raycast : MonoBehaviour{
+    [SerializeField] private Text _regionname = null;
+
+    void Update() {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * 999, Color.green);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 999)) {
+            if (hit.collider.gameObject.tag == "Region") {
+                if (_regionname.text != hit.transform.name) {
+                    _regionname.text = hit.transform.name;
+                }
+            }
+            else {
+                if(_regionname.text != "") {
+                    _regionname.text = "";
+                }
+            }
+        }
+        else {
+            if (_regionname.text != "") {
+                _regionname.text = "";
+            }
+        }
+    }
+}
