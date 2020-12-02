0ef0c86101155fcf66507f630e5a9303501eeaae
chel <c.l.webster@live.com>
2020-12-02 00:43:50 +0000

    sdfgre
    e

---
+++ Assets/RotateOverTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOverTime : MonoBehaviour {
+
+    [SerializeField] float _hoursPerSecond = 1f;        
+    private const float DEGREES_IN_HOUR = 15f;
+    
+    void Update() {
+        transform.Rotate(Vector3.up, DEGREES_IN_HOUR * _hoursPerSecond * Time.deltaTime);
+    }
+}
---
+++ Assets/RotatePlanet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+    [SerializeField] private GameObject _globe = null;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        transform.Rotate(0, yInput, 0, Space.World);
+        _globe.transform.Rotate(xInput, 0, 0, Space.World);
+    }
+}
